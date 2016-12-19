c0824a422293ac4e9febaf524aac7f83a51e1963
Eric Krause <krause197@gmail.com>
2016-12-19 11:35:50 -0800

    add touch event to lower part of screen to end game thread

--- app/src/main/java/com/epicodus/krause197/marbsocc/MainGamePanel.java
+++ app/src/main/java/com/epicodus/krause197/marbsocc/MainGamePanel.java
@@ -1,7 +1,9 @@
 package com.epicodus.krause197.marbsocc;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -10,6 +12,7 @@ import android.view.SurfaceView;
  * Created by Guest on 12/19/16.
  */
 public class MainGamePanel extends SurfaceView implements SurfaceHolder.Callback {
+    private static final String TAG = MainGamePanel.class.getSimpleName();
 
     private MainThread thread;
 
@@ -54,6 +57,14 @@ public class MainGamePanel extends SurfaceView implements SurfaceHolder.Callback
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            if (event.getY()> getHeight() - 50) {
+                thread.setRunning(false);
+                ((Activity)getContext()).finish();
+            } else {
+                Log.d(TAG, "Coords: x=" + event.getX() + ",y=" + event.getY());
+            }
+        }
         return super.onTouchEvent(event);
     }
 
