08c37816cb026538398503c42a5e7f533712c50d
Eric Krause <krause197@gmail.com>
2016-12-20 17:23:21 -0800

    updated main activity to log game loop

--- app/src/main/java/com/epicodus/krause197/marbsocc/MainActivity.java
+++ app/src/main/java/com/epicodus/krause197/marbsocc/MainActivity.java
@@ -2,6 +2,9 @@ package com.epicodus.krause197.marbsocc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
@@ -9,6 +12,21 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(new MainGamePanel(this));
+        Log.d(TAG, "View Added");
+    }
+
+    @Override
+    protected void  onDestroy() {
+        Log.d(TAG, "Destroying ....");
+        super.onDestroy();;
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "Stopping ... ");
+        super.onStop();
     }
 }
